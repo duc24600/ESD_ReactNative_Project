15fd930d1b58e44f05e78f7e36ef99343ab10013
duc24600 <duc24600@gmail.com>
2023-06-12 14:40:33 +0700

    remove navigator container in home screen

--- src/navigators/BottomNavigator.tsx
+++ src/navigators/BottomNavigator.tsx
@@ -1,6 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import React, { useState } from "react";
+import React from "react";
 import Counter from "../screens/Counter";
 import List from "../screens/FlatList";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -10,12 +9,10 @@ const Stack = createStackNavigator();
 
 function BottomNavigator() : JSX.Element {
   return (  
-    <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Counter" component={Counter}/>
         <Tab.Screen name="List" component={List}/>
       </Tab.Navigator>
-    </NavigationContainer>
   );
 }
 
